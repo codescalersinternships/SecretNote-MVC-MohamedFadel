8cb53d90d2e5f7a23165ec71902e1a81a19ed568
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-08 12:04:12 +0300

    feat: implement create_note view to handle note creation with optional expiration

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from .form import SecretNoteForm
+from .models import SecretNote
+
+
+def create_note(request):
+    if request.method == "POST":
+        form = SecretNoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+
+            if form.cleaned_data["expiration_hours"]:
+                note.expiration_time = timezone.now() + timezone.timedelta(
+                    hours=form.cleaned_data["expiration_hours"]
+                )
+            note.save()
+
+            return render(request, "note_created.html", {"note": note})
+
+    else:
+        form = SecretNoteForm()
+    return render(request, "create_note.html", {"form": form})
