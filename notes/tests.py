007fa93fb96f6c1f322619c22ee226dda371fbee
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-10-10 13:39:55 +0300

    test: add tests for user authentication, note creation, and note viewing functionality

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
 from django.utils import timezone
@@ -25,6 +26,15 @@ class SecretNoteModelTest(TestCase):
         )
         self.assertFalse(valid_note.is_expired())
 
+    def test_increment_view(self):
+        note = SecretNote.objects.create(content="Test note", max_views=2)
+        self.assertEqual(note.views, 0)
+        note.increment_view()
+        self.assertEqual(note.views, 1)
+        note.increment_view()
+        self.assertEqual(note.views, 2)
+        self.assertTrue(note.is_expired())
+
 
 class SecretNoteFormTest(TestCase):
     def test_valid_form(self):
@@ -40,10 +50,12 @@ class SecretNoteFormTest(TestCase):
 
 
 class NoteCreationViewTest(TestCase):
-    def setUP(self):
+    def setUp(self):
         self.client = Client()
+        self.user = User.objects.create_user(username="testuser", password="12345")
 
-    def test_create_note(self):
+    def test_create_note_authenticated(self):
+        self.client.login(username="testuser", password="12345")
         response = self.client.post(
             reverse("create_note"),
             {"content": "Test note content", "max_views": 3, "expiration_hours": 24},
@@ -51,6 +63,14 @@ class NoteCreationViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Your Secret Note has been created")
         self.assertEqual(SecretNote.objects.count(), 1)
+
+    def test_create_note_unauthenticated(self):
+        response = self.client.post(
+            reverse("create_note"),
+            {"content": "Test note content", "max_views": 3, "expiration_hours": 24},
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(SecretNote.objects.count(), 0)
 
 
 class NoteViewTest(TestCase):
@@ -67,3 +87,67 @@ class NoteViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "This note has now been deleted")
         self.assertEqual(SecretNote.objects.count(), 0)
+
+    def test_view_expired_note(self):
+        expired_note = SecretNote.objects.create(
+            content="Expired note",
+            expiration_time=timezone.now() - timezone.timedelta(hours=1),
+        )
+        response = self.client.get(reverse("view_note", args=[expired_note.url_key]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Note Expired")
+        self.assertEqual(SecretNote.objects.count(), 1)
+
+
+class IndexViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_view(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Create a New Note")
+
+    def test_index_view_with_url_key(self):
+        note = SecretNote.objects.create(content="Test note content")
+        response = self.client.get(reverse("index") + f"?url_key={note.url_key}")
+        self.assertRedirects(response, reverse("view_note", args=[note.url_key]))
+
+
+class UserAuthTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse("register")
+        self.login_url = reverse("login")
+        self.logout_url = reverse("logout")
+        self.user = User.objects.create_user(username="testuser", password="12345")
+
+    def test_user_registration(self):
+        response = self.client.post(
+            self.register_url,
+            {
+                "username": "newuser",
+                "password1": "testpass123",
+                "password2": "testpass123",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username="newuser").exists())
+
+    def test_user_login(self):
+        response = self.client.post(
+            self.login_url,
+            {
+                "username": "testuser",
+                "password": "12345",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_logout(self):
+        self.client.login(username="testuser", password="12345")
+        response = self.client.post(self.logout_url)
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get(reverse("create_note"))
+        self.assertEqual(response.status_code, 302)
